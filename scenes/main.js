ae830b988d4be7e14cef813bd3a6810183ef5f49
Andrew Wooldridge <triptych@gmail.com>
2021-05-23 02:58:43 +0000

    Initial commit

---
+++ scenes/main.js
@@ -0,0 +1,17 @@
+
+// initialize kaboom context
+const k = kaboom();
+
+// define a scene
+k.scene("main", () => {
+
+    // add a text at position (100, 100)
+    k.add([
+        k.text("ohhimark", 32),
+        k.pos(100, 100),
+    ]);
+
+});
+
+// start the game
+k.start("main");
